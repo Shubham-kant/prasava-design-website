8edb1b04d5efbfd36410aac5fab76be82102ffd7
Shubham-Kant <pro.shubham1999@gmail.com>
2019-10-03 17:41:33 +0530

    set up static files

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express=require('express');
 const port=8000;
 const app=express();
 
-//importing layout module and use it as layout middleware
+//importing layout module 
 const expressLayouts=require('express-ejs-layouts');
+//importing sass middleware and using it
+const sassMiddleware=require('node-sass-middleware');
+app.use(sassMiddleware({
+    src:'./assets/scss',
+    dest:'./assets/css',
+    debug:true,
+    outputStyle:'extended',
+    prefix:'./css'
+
+}));
+app.use(express.static('assets'));
+//extract style and script from subpages into layouts
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+
+//use it as layout middleware
 app.use(expressLayouts);
 
 //use express router
